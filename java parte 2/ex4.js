5ab2f2be605c3be79dfa96ce4ae02b094ec9199d
MARIA JULIA OLIVEIRA DA SILVA <52143758839@info.local>
2024-05-15 10:17:47 -0300

    feat: resto

--- java parte 2/ex4.js
+++ java parte 2/ex4.js
@@ -1,11 +1,19 @@
-//4. Faça um programa que imprima na tela a tabuada de um número qualquer escolhido pelo usuário até o 10.
+// Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
 
-let numero = prompt("Digite um numero ate 10:")
 
-for (let num = 0; num <= 10; num++) {
-    if (num * 1, num *2, num *3, num *4, nu){
-        console.log (num)
+let numero = parseInt(prompt("Digite um número para ver na tabuada: "));
+
+
+if (!isNaN(numero)) {
+    for (let cont = 1; cont <= 10; cont++) {
+        console.log(numero + " X " + cont + " = " + (numero * cont));
     }
+
+}else{
+    console.log("Número invalido. Por favor, digite um número válido.")
 }
-    
+
+console.log(isNaN("Hello Word")); // retorna true 
+console.log(isNaN(123)); // retorna false 
 
--- java parte 2/ex5.js
+++ java parte 2/ex5.js
@@ -1,1 +1,20 @@
-//5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+// Imprimir os primeiros 10 números da sequência de Fibonacci.
+
+//Inicializar os primeiros dois números da sequência deFibonacci.
+let fib1 = 1;
+let fib2 = 1;
+
+//Imprime os primeiros 10 números da sequência de Fibonacci.
+console.log("Os primeiros 10 números da sequência de Fibonacci são: ");
+console.log(fib1);
+console.log(fib2);
+
+//Itera para calcular e imprimir os próximos 8 números da sequência.
+for(let i = 2; i < 10; i++){
+    let nextFib = fib1 + fib2;
+    console.log(nextFib);
+
+    //Atualizar os valores de fib1 e fib2 para os proximos calculos
+    fib1 = fib2;
+    fib2 = nextFib;
+}
